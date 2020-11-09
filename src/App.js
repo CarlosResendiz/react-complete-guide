ed2dfecd5610d3b36cae8c603176196de4fc239e
Carlos Resendiz <resendiz.ca@gmail.com>
2020-11-08 23:06:28 -0600

    using state and extending App using Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+// function App() {  
+class App extends Component {  
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <h1>This is working</h1>
-      <Person name="Max" age="28"></Person>
-      <Person name="Manu" age="29">My Hobbies: Racing</Person>
-      <Person name="Stephanie" age="26"></Person>
-    </div>
-  );
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26},
+    ]
+  }
+
+  render() {
+    return ( 
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <h1>This is working</h1>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div>
+     );
+  };
 }
 
 export default App;
